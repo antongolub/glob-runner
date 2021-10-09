a0439f2141d81ad78e74a9671b00ed27a86a6126
Anton Golub <antongolub@antongolub.com>
2021-10-09 22:10:50 +0300

    fix(types): fix libdefs

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,10 @@
 declare module 'glob-runner' {
   type ICallable<A extends any[] = any[], R = any> = (...args: A) => R
 
-  export const run: (pattern: string | string[], cwd?: string, cb?: ICallable) => Promise<void>
+  export const run: (options?: {
+    pattern: string | string[],
+    cwd?: string,
+    cb?: ICallable,
+    nodeVersion?: string
+  }) => Promise<void>
 }
